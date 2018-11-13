be0e843e82f0c58d2fc8ceb7e1e1dd2b049716b9
Julien Perrenoud <julien.perrenoud@jfperren.io>
2018-11-13 13:13:34 +0100

    Fix some things in sync.sh [WIP]

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,14 +6,15 @@ REMOTE_DIR="~/ADAProject"
 SSH_PW_FILE=".ssh-pw"
 
 # Create zip with dependencies
-zip -r src.zip src
-mv src.zip jobs/src.zip
+# zip -r src.zip src
+# mv src.zip jobs/src.zip
 
 # Copy content of "jobs" folder
 scp -i "~/.ssh/ada_rsa" -r "jobs" $SSH_USERNAME@$SSH_URL:$REMOTE_DIR
+scp -i "~/.ssh/ada_rsa" -r "src" $SSH_USERNAME@$SSH_URL:$REMOTE_DIR
 
 # Remove zip
-rm jobs/src.zip
+# rm jobs/src.zip
 
 # scp -i "~/.ssh/ada_rsa" -r "src" $SSH_USERNAME@$SSH_URL:$REMOTE_DIR
 # scp -i "~/.ssh/ada_rsa" -r "./{scripts,jobs}" $SSH_USERNAME@$SSH_URL:$REMOTE_DIR
