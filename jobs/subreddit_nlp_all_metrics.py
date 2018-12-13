bd0a1991c3d0f00e7b4c8403d46965acc7d08c07
Valentin Borgeaud <valentin.borgeaud@epfl.ch>
2018-12-13 23:02:04 +0100

    Fix job to compute nlp metrics on subreddit

--- jobs/subreddit_nlp_all_metrics.py
+++ jobs/subreddit_nlp_all_metrics.py
@@ -24,12 +24,11 @@ from textblob.sentiments import NaiveBayesAnalyzer
 from collections import Counter
 import os
 import numpy as np
-import pickle
+import datetime as dt
 
 ###
 ###
 ###
-
 
 # Create spark session
 spark = SparkSession.builder.getOrCreate()
@@ -47,10 +46,10 @@ messages = messages.withColumn('created_utc', func.from_unixtime(messages['creat
                                 .withColumnRenamed('created_utc', 'creation_date')
 
 # Filter per subreddit
-with open('subreddits_set/sports_subreddits.pkl', 'rb') as f:
-    subreddits_set = pickle.load(f, encoding='utf-8')
+subreddits_set = set(sc.textFile('subreddits_txt/political_subreddits.txt').collect())
 messages.withColumn('subreddit', func.udf(lambda s: s.lower(), StringType())(func.col('subreddit')))
 messages = messages.filter(func.col('subreddit').isin(subreddits_set))
+#messages = messages.sample(False, 0.3, 0)
 
 # Clean messages
 cleaned_messages = messages.filter("body != '[removed]' and body != '[deleted]'")
@@ -77,8 +76,35 @@ df_count_matches(bw_1_grams, 'bw_count_matches')
 df_count_matches(hw_1_grams, 'hw_count_matches')
 df_count_matches_intensity(hw_ref_1_grams, hw_ref_1_intensity, 'hw_ref_count_matches')
 
-nlp_metrics_df = cleaned_messages.selectExpr('id', 'subreddit', 'subreddit_id', 'creation_date', 'body', 'process_body(body) as tokens')
-nlp_metrics_df = nlp_metrics_df.selectExpr('id', 'subreddit', 'subreddit_id', 'creation_date', 'body', "compute_nltk_polarity(body) as nltk_scores", "compute_blob_polarity(body) as blob_scores", "bw_count_matches(tokens) as nb_bw_matches", "hw_count_matches(tokens) as nb_hw_matches", "hw_ref_count_matches(tokens) as hw_ref_matches")
-nlp_metrics_df = nlp_metrics_df.selectExpr('id', 'subreddit', 'subreddit_id', 'creation_date', 'body', 'nltk_scores.neg as nltk_negativity', 'nltk_scores.neu as nltk_neutrality', 'nltk_scores.pos as nltk_positivity', 'blob_scores.polarity as text_blob_polarity', 'blob_scores.subjectivity as text_blob_subjectivity', 'nb_bw_matches', 'nb_hw_matches', 'hw_ref_matches.intensity as hw_ref_intensity', 'hw_ref_matches.count as nb_hw_ref_matches')
+# nlp_metrics_df = cleaned_messages.selectExpr('id', 'subreddit', 'subreddit_id', 'creation_date', 'body', 'process_body(body) as tokens')
+# nlp_metrics_df = nlp_metrics_df.selectExpr('id', 'subreddit', 'subreddit_id', 'creation_date', 'body', "compute_nltk_polarity(body) as nltk_scores", "compute_blob_polarity(body) as blob_scores", "bw_count_matches(tokens) as nb_bw_matches", "hw_count_matches(tokens) as nb_hw_matches", "hw_ref_count_matches(tokens) as hw_ref_matches")
+# nlp_metrics_df = nlp_metrics_df.selectExpr('id', 'subreddit', 'subreddit_id', 'creation_date', 'body', 'nltk_scores.neg as nltk_negativity', 'nltk_scores.neu as nltk_neutrality', 'nltk_scores.pos as nltk_positivity', 'blob_scores.polarity as text_blob_polarity', 'blob_scores.subjectivity as text_blob_subjectivity', 'nb_bw_matches', 'nb_hw_matches', 'hw_ref_matches.intensity as hw_ref_intensity', 'hw_ref_matches.count as nb_hw_ref_matches')
 
-nlp_metrics_df.write.mode('overwrite').parquet('subreddit_sports_nlp.parquet')
+# nlp_metrics_df.write.mode('overwrite').parquet('subreddit_sports_nlp.parquet')
+
+nlp_metrics_df = cleaned_messages.selectExpr('subreddit', 'subreddit_id', 'creation_date', 'body', 'process_body(body) as tokens')
+nlp_metrics_df = nlp_metrics_df.selectExpr('subreddit', 'subreddit_id', 'creation_date', 'body', "compute_nltk_polarity(body) as nltk_scores", "compute_blob_polarity(body) as blob_scores", "bw_count_matches(tokens) as nb_bw_matches", "hw_count_matches(tokens) as nb_hw_matches", "hw_ref_count_matches(tokens) as hw_ref_matches")
+nlp_metrics_df = nlp_metrics_df.selectExpr('subreddit', 'subreddit_id', 'creation_date', 'nltk_scores.neg as nltk_negativity', 'nltk_scores.neu as nltk_neutrality', 'nltk_scores.pos as nltk_positivity', 'blob_scores.polarity as text_blob_polarity', 'blob_scores.subjectivity as text_blob_subjectivity', 'nb_bw_matches', 'nb_hw_matches', 'hw_ref_matches.intensity as hw_ref_intensity', 'hw_ref_matches.count as nb_hw_ref_matches')
+
+nlp_metrics_df.registerTempTable("nlp_metrics")
+subreddit_metrics = spark.sql("""
+SELECT
+    subreddit,
+    subreddit_id,
+    MIN(creation_date) AS min_date,
+    MAX(creation_date) AS max_date,
+    COUNT(*) AS msg_count,
+    SUM(nltk_negativity) AS sum_nltk_neg,
+    SUM(nltk_neutrality) AS sum_nltk_neu,
+    SUM(nltk_positivity) AS sum_nltk_pos,
+    SUM(text_blob_polarity) AS sum_blob_pol,
+    SUM(text_blob_subjectivity) AS sum_blob_subj,
+    SUM(nb_bw_matches) AS sum_bw,
+    SUM(nb_hw_matches) AS sum_hw,
+    SUM(nb_hw_ref_matches) AS sum_hw_ref,
+    SUM(hw_ref_intensity) AS sum_hw_ref_intensity
+FROM nlp_metrics
+GROUP BY subreddit, subreddit_id
+""")
+
+subreddit_metrics.write.mode('overwrite').parquet(f'subreddit_political_nlp_0.3_{dt.datetime.now()}.parquet')
